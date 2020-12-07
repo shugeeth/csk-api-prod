754ce55ad0fe81c8ac4223f682dbe5b8884ccd5c
Shugeeth Babu <meetshugeeth@gmail.com>
2020-12-07 23:34:28 +0530

    CORS Issue

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 
 app.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Origin','https://shugeeth.github.io');
+    // res.header('Access-Control-Allow-Origin','https://shugeeth.github.io');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     next();
 })
 
