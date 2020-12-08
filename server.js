543eb5b3c6164ff75a0bca3c0409cfc63ec86f97
Shugeeth Babu <meetshugeeth@gmail.com>
2020-12-08 12:58:01 +0530

    Added allowCrossDomain variable

--- server.js
+++ server.js
@@ -7,16 +7,18 @@ const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 
-app.use((req,res,next)=>{
+var allowCrossDomain = function(req,res,next){
     // res.header('Access-Control-Allow-Origin','https://shugeeth.github.io');
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     next();
-})
+}
+
+app.use(allowCrossDomain)
 
 //To remove CORS policy error
-app.use(cors());
+//app.use(cors());
 
 //Pool to connect to DB
 const pool = require("./db")
